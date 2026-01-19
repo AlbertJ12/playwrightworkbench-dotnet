ef7b36d73ad472550c5b0ad40c5b80572c33eabd
Albert Colon <albertjoel.cg@gmail.com>
2026-01-19 12:41:00 -0400

    Initial Test

---
+++ UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+namespace PlaywrightTests;
+
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [SetUp]
+    public async Task Setup()
+    {
+        await Context.Tracing.StartAsync(new()
+        {
+            Title = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}",
+            Screenshots = true,
+            Snapshots = true,
+            Sources = true
+        });
+
+        await Page.GotoAsync("https://playwright.dev");
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Context.Tracing.StopAsync(new()
+        {
+            Path = Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                "playwright-traces",
+                $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+            )
+        });
+    }
+
+
+    [Test]
+    public async Task MainNavigation()
+    {
+        // Assertions use the expect API.
+        await Page.PauseAsync();
+        await Expect(Page).ToHaveURLAsync("https://playwright.dev/");
+    }
+}
